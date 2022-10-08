c4ec5cf62aaecf2ed07c53032e8d8b629b7e4748
Sungho <hurgj123kr@gmail.com>
2022-10-08 16:44:38 +0900

    my admin interface part1

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+PROJECT_APPS = ["users.apps.UsersConfig",]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -116,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "users.User"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("",include("core.urls", namespace="core")),
-    path("lists/",include("lists.urls", namespace="lists")),
-    path("rooms/",include("rooms.urls", namespace="rooms")),
-    path("users/",include("users.urls", namespace="users")),
-    path("reviews/",include("reviews.urls", namespace="reviews")),
-    path("reservations/",include("reservations.urls", namespace="reservations")),
-    path("conversations/",include("conversations.urls", namespace="conversations")),
+    # path("",include("core.urls", namespace="core")),
+    # path("lists/",include("lists.urls", namespace="lists")),
+    # path("rooms/",include("rooms.urls", namespace="rooms")),
+    # path("users/",include("users.urls", namespace="users")),
+    # path("reviews/",include("reviews.urls", namespace="reviews")),
+    # path("reservations/",include("reservations.urls", namespace="reservations")),
+    # path("conversations/",include("conversations.urls", namespace="conversations")),
     path('admin/', admin.site.urls),
 ]
