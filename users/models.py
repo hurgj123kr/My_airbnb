18201dfa5a78f93a593b4d8582d9f004623df1ae
Sungho <hurgj123kr@gmail.com>
2022-10-09 19:08:43 +0900

    admin user private data birthdate language currency superhost add

--- users/models.py
+++ users/models.py
@@ -2,17 +2,37 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    """ Custom User Model """
+
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
-    
+
     GENDER_CHOICES = (
         (GENDER_MALE, "Male"),
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
-    avatar = models.ImageField(null=True,blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES,max_length=10, blank=True, null=True)
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "ko"
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
     bio = models.TextField(default="")
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    birthdate = models.DateField(null=True, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES,max_length=5, null=True, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=5, null=True)
+    superhost = models.BooleanField(default=False)
 
