2542cf425673e11f8d9a8dc684a582b648c45bbf
Sungho <hurgj123kr@gmail.com>
2022-10-13 13:42:44 +0900

    room models complete?

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,6 +4,7 @@ from . import models
 
 @admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
     """ Item Admin Definition """
     pass
 
@@ -12,4 +13,11 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    pass
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
+    
     pass
