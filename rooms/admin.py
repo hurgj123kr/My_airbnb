316fbfe07ab0b3343ea09931d2981e7d9404e84b
Sungho <hurgj123kr@gmail.com>
2022-10-19 19:39:40 +0900

    count_photos and used_by add

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,7 +6,11 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
-    pass
+    
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
@@ -45,6 +49,7 @@ class RoomAdmin(admin.ModelAdmin):
         "room_type",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -64,6 +69,9 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):
         return obj.amenities.count()
+    
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
