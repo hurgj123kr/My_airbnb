2c63f622f55f8fe16e5a2d9421f767b1912e5fa0
Sungho <hurgj123kr@gmail.com>
2022-10-24 10:05:32 +0900

    MEDIA URL and settings.DEBUG True urlpatterns changes

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,8 @@ AUTH_USER_MODEL = "users.User"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
+MEDAA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
