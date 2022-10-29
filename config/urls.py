51d8e36404f547b751d8a2066c3e08eb29dfa93b
Sungho <hurgj123kr@gmail.com>
2022-10-29 15:34:08 +0900

    get_thumbnail function add

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from dango.conf.urls.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path("",include("core.urls", namespace="core")),
@@ -29,5 +29,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+#개발 환경이 맞을경우 이미지 저장경로는 MEDIA_URL이며, 폴더는 MEDOA_ROOT로 함.
 if settings.DEBUG:
-    urlpatterns = static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
