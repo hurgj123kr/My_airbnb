388a7626571a984d30a381732eb8e160445fdef0
Sungho <hurgj123kr@gmail.com>
2022-10-31 14:13:50 +0900

    rooms management commands seed_amenities create

---
+++ rooms/management/commands/seed_amenities.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from rooms.models import Amenity
+
+
+class Command(BaseCommand):
+
+    """ Command to Definition """
+
+    help = "This command auto create amenities "
+
+    """     def add_arguments(self, parser):
+    parser.add_argument(
+        "--times", help="How many create amenities"
+    )
+    """
+
+    def handle(self,*args, **options):
+        amenities = [
+            "Wifi",
+            "Kitchen",
+            "Washer",
+            "Dryer",
+            "Air conditioning",
+            "Heating",
+            "Dedicated workspace",
+            "TV",
+            "Hair dryer",
+            "Iron",
+            "Pool",
+            "Hot tub",
+            "Free parking on premises",
+            "EV charger",
+            "Crib",
+            "Gym",
+            "BBQ grill",
+            "Breakfast",
+            "Indoor fireplace",
+            "Smoking allowed",
+            "Smoke alarm",
+            "Carbon monoxide alarm",
+        ]
+        
+        for i in amenities:
+            Amenity.objects.create(name=i)
+        self.stdout.write(self.style.SUCCESS("Amenities created!!"))
+
