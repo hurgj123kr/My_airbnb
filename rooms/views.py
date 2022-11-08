f6bb8ccd536a8f270b76bc490eff7fbacb132a70
Sungho <hurgj123kr@gmail.com>
2022-11-08 16:10:24 +0900

    test home hello create all_rooms.html

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render
 
 def all_rooms(request):
     
-    return render(request, "all_rooms")
+    now = datetime.now()
+
+    return render(request, "all_rooms.html",{"now": now})
