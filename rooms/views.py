b8d9794c657ab69840b03ed775eb9832946ffbc7
Sungho <hurgj123kr@gmail.com>
2022-11-09 13:46:27 +0900

    page_number limit big over_page reirect go home

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,9 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 def all_rooms(request):
+    
     # Python만 사용해서  page 구현방식
     # page = request.GET.get("page", 1)
     # page = int(page or 1)
@@ -16,5 +16,9 @@ def all_rooms(request):
     page = request.GET.get("page",1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-    return render(request, "rooms/home.html", context={"page": rooms,},)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", context={"page": rooms,},)
+    except EmptyPage:
+        return redirect("/")
+
