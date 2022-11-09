9840a6bdfa5c0a90b192e667154886b96a826a17
Sungho <hurgj123kr@gmail.com>
2022-11-09 12:55:19 +0900

    python rooms views page limit 10 view

--- rooms/views.py
+++ rooms/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from . import models as room_models
 
 def all_rooms(request):
-    all_rooms = room_models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = room_models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={"all_rooms": all_rooms})
