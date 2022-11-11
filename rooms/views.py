f25ded2e51ed85ceb6e584cb1f948d14c4a16968
Sungho <hurgj123kr@gmail.com>
2022-11-11 18:38:05 +0900

    search paginator create

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models, forms
 
 
@@ -82,7 +83,14 @@ class SearchView(View):
                 for facility in facilities:
                     filter_args["facilities"] = facility
 
-                rooms = models.Room.objects.filter(**filter_args)
+                qs = models.Room.objects.filter(**filter_args).order_by("created")
+
+                paginator = Paginator(qs, 10, orphans=5)
+
+                page = request.GET.get("page", 1)
+
+                rooms = paginator.get_page(page)
+
 
                 return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
 
