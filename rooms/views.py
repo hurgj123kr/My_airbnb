7a1083d8bccbf02d47a15395c1e18e7ad8ffbaa5
Sungho <hurgj123kr@gmail.com>
2022-11-11 11:51:31 +0900

    search create

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -16,6 +17,12 @@ class RoomDetail(DetailView):
 
     """ Room Detail Definition """
     model = models.Room
+
+def search(request):
+    city = request.GET.get('city')
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", context={"city": city})
+
 
 # 함수형 detailview
 # def room_detail(request, pk):
