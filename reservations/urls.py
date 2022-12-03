5b4de45879839175358ea3da9719211a201c70f6
Sungho <hurgj123kr@gmail.com>
2022-12-03 19:57:03 +0900

    reservation detail part2

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "reservations"
 
 urlpatterns = [
-    path("create/<int:room>/<int:year>-<int:month>-<int:day>/", views.create, name="create"),
+    path("create/<int:room>/<int:year>-<int:month>-<int:day>", views.create, name="create"),
     path("<int:pk>/", views.ReservationDetailView.as_view(), name="detail"),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,4 +1,5 @@
 import datetime
+from django.http import Http404
 from django.views.generic import View
 from django.contrib import messages
 from django.shortcuts import render, reverse, redirect
@@ -14,23 +15,26 @@ def create(request, room, year, month, day):
     try:
         date_obj = datetime.datetime(year, month, day)
         room = room_models.Room.objects.get(pk=room)
-        models.Reservation.objects.get(day=date_obj, reservation__room=room)
+        models.BookedDay.objects.get(day=date_obj, reservation__room=room)
         raise CreateError()
     except (room_models.Room.DoesNotExist, CreateError):
         messages.error(request, "Can't Reserve That Room")
         return redirect(reverse("core:home"))
     except models.BookedDay.DoesNotExist:
-        reservation = models.BookedDay.objects.create(
-            guset = request.user,
-            room = room,
-            check_in = date_obj,
-            check_out = date_obj + datetime.timedeltae(days=1),
+        reservation = models.Reservation.objects.create(
+            guest=request.user,
+            room=room,
+            check_in=date_obj,
+            check_out=date_obj + datetime.timedelta(days=1),
         )
-        return redirect(reverse("reservations:detail", kwargs={'pk': reservation.pk }))
+        return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
+
 
 class ReservationDetailView(View):
-    def get(self, pk):
+    def get(self, *args, **kwargs):
+        pk = kwargs.get("pk")
         reservation = models.Reservation.objects.get_or_none(pk=pk)
-        if not reservation:
-            return redirect(reverse("core:home"))
+        if not reservation or (reservation.guest != self.request.user and reservation.room.host != self.request.user):
+            raise Http404()        
+        return render(self.request, "reservations/detail.html", {"reservation": reservation})
          
