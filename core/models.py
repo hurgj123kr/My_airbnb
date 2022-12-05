b5c0a88ae1ad799fdc0fd0f531754632b8ec2fd4
Sungho <hurgj123kr@gmail.com>
2022-12-05 11:20:07 +0900

    reservation review create part1

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from . import managers
 
 class TimeStampedModel(models.Model):
     """ Time Stampe Model """
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomModelManager()
     
     # database안에 데이터가 저장되지 않도록 해줌.
     class Meta:
