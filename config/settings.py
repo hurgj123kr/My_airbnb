7108a6f0463adbbb69fcf759dbbd862523f6a82b
Sungho <hurgj123kr@gmail.com>
2022-12-07 13:31:38 +0900

    translation add

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,7 @@ LOGIN_URL = "/users/login/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#locale
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
