f7e5b5d508d2ef0f1444e29b7208d21a48b089b1
Sungho <hurgj123kr@gmail.com>
2023-01-05 15:39:27 +0900

    boto3 and awsebcli error resolve collectstatic error

--- config/settings.py
+++ config/settings.py
@@ -185,7 +185,7 @@ if not DEBUG:
     
     DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
     STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
-    AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID")
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "myair-bnb"
     AWS_DEFAULT_ACL = "public-read"
