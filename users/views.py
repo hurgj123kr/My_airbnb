7b0967cf45497da88d51105b6d1dac07c6abf31d
Sungho <hurgj123kr@gmail.com>
2023-01-07 11:16:30 +0900

    social login update

--- users/views.py
+++ users/views.py
@@ -95,7 +95,7 @@ def verification_email(request, key):
 
 def github_login(request):
     client_id = os.environ.get("GH_ID")
-    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback/"
+    redirect_uri = "http://mybnb-test.eba-vs2apwam.ap-northeast-2.elasticbeanstalk.com/users/login/github/callback"
     return redirect(f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user")
 
 class GithubException(Exception):
@@ -161,7 +161,7 @@ def github_callback(request):
 
 def kakao_login(request):
     REST_API_KEY = os.environ.get("KAKAO_ID")
-    REDIRECT_URI = "http://127.0.0.1:8000/users/login/kakao/callback/"
+    REDIRECT_URI = "http://mybnb-test.eba-vs2apwam.ap-northeast-2.elasticbeanstalk.com/users/login/kakao/callback/"
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code"
     )
