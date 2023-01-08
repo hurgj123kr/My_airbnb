6d61f4480dd3a540416c1e29dce249790357099e
Sungho <hurgj123kr@gmail.com>
2023-01-08 11:14:46 +0900

    social login kakao error part2

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET", "7xx%r5auJw1jJva")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com","127.0.0.1"]
 
