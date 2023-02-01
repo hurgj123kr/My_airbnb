df8fcc52869346367d8b749d83e90bcc443c85f4
Sungho <hurgj123kr@gmail.com>
2023-02-01 18:39:08 +0900

    s3 name change

--- config/settings.py
+++ config/settings.py
@@ -187,7 +187,7 @@ if not DEBUG:
     STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "my-bnb"
+    AWS_STORAGE_BUCKET_NAME = "myair-test"
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400",}
 
