cb7cd6d7526d5b69e47eec45b4d987b4c6d2e4a1
Sungho <hurgj123kr@gmail.com>
2023-02-06 13:31:52 +0900

    error resolve

--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, View, UpdateView, FormVie
 from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin 
 from users import mixins as user_mixins
@@ -19,15 +20,11 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
+@method_decorator(login_required)
 class RoomDetail(DetailView):
 
     """ Room Detail Definition """
     model = models.Room
-    template_name="rooms/room_detail.html"
-     
-    @login_required
-    def get(self, request):
-        detail = super().get(self,request)
         
 
 
